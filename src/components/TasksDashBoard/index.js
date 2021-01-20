e78086ed55dcad8ca6b720115a02ccbdf9b3f4e7
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-21 01:04:56 +0300

    Added task dashboard markup

--- src/components/TasksDashBoard/index.js
+++ src/components/TasksDashBoard/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-
+import Sidebar from "./SideBar";
+import Tasks from "../Tasks";
 import "./style.css";
 
 function TaskDashBoard(props) {
@@ -7,8 +8,13 @@ function TaskDashBoard(props) {
     <div className="task-dashboard">
       <TopHeader />
       <div className="row">
-        <div className="col-md-2 col-lg-3"></div>
-        <div className="col-md-10 col-lg-9"></div>
+        <div className="col-md-2 col-lg-3">
+          <Sidebar />
+        </div>
+        <div className="col-md-10 col-lg-9">
+          <div className="dashboard-secondary-header"></div>
+          <Tasks />
+        </div>
       </div>
     </div>
   );
