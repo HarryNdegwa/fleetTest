0925e8af9c656098ced42db0ca95427880c21aeb
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-21 00:25:47 +0300

    Routed tasks wrapper component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import "./App.css";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
-import Tasks from "./components/Tasks";
+import TasksWrapper from "./components/TasksWrapper";
 
 function App(props) {
   return (
     <div className="App">
       <Switch>
         <Route path="/login" component={Login} />
-        <Route path="/tasks" component={Tasks} />
+        <Route path="/tasks" component={TasksWrapper} />
       </Switch>
     </div>
   );
