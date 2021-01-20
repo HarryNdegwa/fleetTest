1bd676838414794f799183a5c0c7746dbcca7d1a
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-21 00:52:45 +0300

    Added task dashboard component

---
+++ src/components/TasksDashBoard/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function TaskDashBoard(props) {
+  return (
+    <div className="task-dashboard">
+      <TopHeader />
+      <div className="row">
+        <div className="col-md-2 col-lg-3"></div>
+        <div className="col-md-10 col-lg-9"></div>
+      </div>
+    </div>
+  );
+}
+
+export default TaskDashBoard;
