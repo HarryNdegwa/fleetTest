3f57ab16b3dc84dde9a4934ce0e801e2e4f013c3
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-21 00:58:29 +0300

    Added sidebar component

---
+++ src/components/TasksDashBoard/SideBar/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Sidebar(props) {
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>Helloo</li>
+        <li>Helloo</li>
+        <li>Helloo</li>
+        <li>Helloo</li>
+        <li>Helloo</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Sidebar;
