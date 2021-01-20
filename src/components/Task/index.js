3875d1489720c87f8c5a0b59ab3c39a1918d20a8
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-20 22:37:09 +0300

    Added task action icons

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -1,11 +1,25 @@
 import React from "react";
-import { ImCheckboxChecked } from "react-icons/im";
+import { ImCheckboxChecked, ImCross } from "react-icons/im";
 import "./style.css";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import { AiFillStar } from "react-icons/ai";
+import { BsTagFill } from "react-icons/bs";
+import "react-datepicker/dist/react-datepicker.css";
 
 function Task(props) {
+  const [startDate, setStartDate] = useState(new Date());
   return (
     <div className="task">
       <ImCheckboxChecked />
+      <input type="text" />
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+      />
+      <AiFillStar />
+      <BsTagFill />
+      <ImCross />
     </div>
   );
 }
