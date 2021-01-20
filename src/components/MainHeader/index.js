9b3c03feaddad7aea2ee4c53757c1ceeb612a8c5
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-20 20:24:54 +0300

    Added main header hamburger and styled it

--- src/components/MainHeader/index.js
+++ src/components/MainHeader/index.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./style.css";
+import HamburgerMenu from "react-hamburger-menu";
 
-function index(props) {
-  return <div className="container-fluid main-header"></div>;
+function MainHeader(props) {
+  const [hamOpen, setHamOpen] = useState(false);
+
+  const handleClick = (e) => {
+    setHamOpen(!hamOpen);
+  };
+
+  return (
+    <div className="container-fluid main-header">
+      <div className="sm-hamburger">
+        <HamburgerMenu
+          isOpen={hamOpen}
+          menuClicked={handleClick}
+          width={15}
+          height={10}
+          color="white"
+          strokeWidth={3}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default index;
+export default MainHeader;
