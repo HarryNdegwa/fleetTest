b631b7129dc826474d9f1f91b0917eae4b8efbb0
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-20 19:37:10 +0300

    Added login component markup

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -3,7 +3,18 @@ import React from "react";
 import "./style.css";
 
 function index(props) {
-  return <div className="container-fluid login"></div>;
+  return (
+    <div className="container-fluid login">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            <h3 className="lead">Log in to save and access your lists</h3>
+          </div>
+          <div className="col-lg-6"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default index;
