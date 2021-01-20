7de317b5497e238bf80a2a56e80c511ca88ba7bc
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-21 01:20:57 +0300

    Some styling

--- src/components/TasksDashBoard/index.js
+++ src/components/TasksDashBoard/index.js
@@ -2,19 +2,22 @@ import React from "react";
 import Sidebar from "./SideBar";
 import Tasks from "../Tasks";
 import "./style.css";
-import TopHeader from "../TopHeader";
 
 function TaskDashBoard(props) {
   return (
     <div className="task-dashboard">
-      <TopHeader />
-      <div className="row">
+      <div className="dashboard-top-header-wrapper">
+        <div className="dashboard-top-header"></div>
+      </div>
+      <div className="row no-gutters">
         <div className="col-md-2 col-lg-3">
           <Sidebar />
         </div>
-        <div className="col-md-10 col-lg-9">
+        <div className="col-md-10 col-lg-9 ">
           <div className="dashboard-secondary-header"></div>
-          <Tasks />
+          <div className="px-2">
+            <Tasks />
+          </div>
         </div>
       </div>
     </div>
