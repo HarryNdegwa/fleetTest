508829e6fed3017920d6ba6113d100af48e2708a
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-21 02:25:07 +0300

    Added links to saved list component

--- src/components/SavedLists/index.js
+++ src/components/SavedLists/index.js
@@ -1,9 +1,42 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import { FaUser } from "react-icons/fa";
+import { AiOutlineBars } from "react-icons/ai";
 import "./style.css";
+import { ImPlus } from "react-icons/im";
 
 function SavedLists(props) {
-  return <div className="saved-lists"></div>;
+  return (
+    <div className="saved-lists">
+      <div className="saved-list-top-header">
+        <p className="mr-3">
+          <span className="mx-1">
+            <FaUser />
+          </span>
+          mike@gmail.com
+        </p>
+
+        <Link to="/" className="db-link mr-3">
+          <span className="mr-1">
+            <AiOutlineBars />
+          </span>
+          My Saved Lists
+        </Link>
+        <Link to="/" className="db-link mr-3">
+          Logout
+        </Link>
+      </div>
+
+      <h6>My Saved Lists</h6>
+
+      <Link to="/">
+        <span>
+          <ImPlus />
+          Create New List
+        </span>
+      </Link>
+    </div>
+  );
 }
 
 export default SavedLists;
