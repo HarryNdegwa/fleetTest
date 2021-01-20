f92a807260d9cfe5184239b995e29abcfbf7a842
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-20 23:34:01 +0300

    Added breakpoints to task component

--- src/components/Task/index.js
+++ src/components/Task/index.js
@@ -14,14 +14,25 @@ function Task(props) {
         <ImCheckboxChecked style={{ fontSize: "30px" }} />
         <div className="task-inner-info">
           <div className="task-inner-info-content">
-            <h6 className="mx-3">
-              Get some code done!Get some code done!Get some code done
-            </h6>
+            <h6 className="mx-3">Get some code done</h6>
           </div>
           <small className="task-date">No due date</small>
         </div>
       </div>
       <div className="task-actions-wrapper">
+        <div className="task-actions">
+          <AiFillStar
+            style={{ fontSize: "25px" }}
+            className="task-action-icon"
+          />
+          <BsTagFill
+            style={{ fontSize: "22px" }}
+            className="task-action-icon"
+          />
+          <ImCross style={{ fontSize: "20px" }} className="task-action-icon" />
+        </div>
+      </div>
+      <div className="task-actions-sm-wrapper">
         <div className="task-actions">
           <AiFillStar
             style={{ fontSize: "25px" }}
