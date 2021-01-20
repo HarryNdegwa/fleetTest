bc0137b107335c99c53d474d79f47f13fb315cc1
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-20 20:30:45 +0300

    Moved hamburger to its own component

---
+++ src/components/MainHeader/Hamburger/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import HamburgerMenu from "react-hamburger-menu";
+
+function Hamburger(props) {
+  const handleClick = (e) => {};
+  return (
+    <div className="sm-hamburger">
+      <HamburgerMenu
+        isOpen={false}
+        menuClicked={handleClick}
+        width={15}
+        height={10}
+        color="white"
+        strokeWidth={3}
+      />
+    </div>
+  );
+}
+
+export default Hamburger;
--- src/components/MainHeader/index.js
+++ src/components/MainHeader/index.js
@@ -1,27 +1,12 @@
 import React, { useState } from "react";
+import Hamburger from "./Hamburger";
 
 import "./style.css";
-import HamburgerMenu from "react-hamburger-menu";
 
 function MainHeader(props) {
-  const [hamOpen, setHamOpen] = useState(false);
-
-  const handleClick = (e) => {
-    setHamOpen(!hamOpen);
-  };
-
   return (
     <div className="container-fluid main-header">
-      <div className="sm-hamburger">
-        <HamburgerMenu
-          isOpen={hamOpen}
-          menuClicked={handleClick}
-          width={15}
-          height={10}
-          color="white"
-          strokeWidth={3}
-        />
-      </div>
+      <Hamburger />
     </div>
   );
 }
