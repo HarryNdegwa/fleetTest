e5544757b8cd264aafb913bff460ba01d3714109
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-21 13:17:48 +0300

    Added updated list tasks action type

--- src/redux/actions/actionsType.js
+++ src/redux/actions/actionsType.js
@@ -5,3 +5,4 @@ export const SAVE_LIST = "SAVE_LIST";
 export const SETUP_TASKS_ARRAY = "SETUP_TASKS_ARRAY";
 export const DELETE_TASK = "DELETE_TASK";
 export const ADD_TASK = "ADD_TASK";
+export const UPDATE_LIST_TASKS = "UPDATE_LIST_TASKS";
