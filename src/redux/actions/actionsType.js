fd6892fcd002e327044b1040075b86fa7549b7f5
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-21 05:05:36 +0300

    Added setup task array action type

--- src/redux/actions/actionsType.js
+++ src/redux/actions/actionsType.js
@@ -2,3 +2,4 @@ export const LOGIN_LOADING = "LOGIN_LOADING";
 export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
 export const LOGIN_ERROR = "LOGIN_ERROR";
 export const SAVE_LIST = "SAVE_LIST";
+export const SETUP_TASK_ARRAY = "SETUP_TASK_ARRAY";
