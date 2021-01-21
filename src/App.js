cd0b19ccf90d280cfba88e0763bdc2891a3a2a43
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-21 12:39:03 +0300

    Secured pages

--- src/App.js
+++ src/App.js
@@ -5,19 +5,34 @@ import TasksWrapper from "./components/TasksWrapper";
 import TasksDashBoard from "./components/TasksDashBoard";
 import SavedLists from "./components/SavedLists";
 import NewList from "./components/NewList";
+import { connect } from "react-redux";
 
 function App(props) {
+  const { isAuth } = props;
   return (
     <div className="App">
       <Switch>
         <Route path="/login" component={Login} />
-        <Route path="/tasks" component={TasksWrapper} />
-        <Route path="/tasks-dashboard" component={TasksDashBoard} />
-        <Route path="/saved-lists" component={SavedLists} />
-        <Route path="/new-list" component={NewList} />
+        <Route
+          path="/tasks-dashboard"
+          render={() =>
+            isAuth ? <TasksDashBoard /> : <Redirect to="/login" />
+          }
+        />
+        <Route
+          path="/saved-lists"
+          render={() => (isAuth ? <SavedLists /> : <Redirect to="/login" />)}
+        />
+        render={() => (isAuth ? <NewList /> : <Redirect to="/login" />)}
       </Switch>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    isAuth: state.loginReducer.isAuth,
+  };
+};
+
+export default connect(mapStateToProps)(App);
