d27c845ede778ed17bdf9d44c209ace7d8abe0d9
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-21 07:23:01 +0300

    Added delete task action

--- src/redux/actions/taskActions.js
+++ src/redux/actions/taskActions.js
@@ -10,3 +10,39 @@ export const setUpTasksArray = (id) => {
     });
   };
 };
+
+const checkCurrent = (data) => {
+  switch (data.current) {
+    case "unStarred":
+      return 1;
+    case "Starred":
+      return 0;
+    case "Checked":
+      return 2;
+  }
+};
+
+export const deleteTask = (data) => {
+  return (dispatch, getState) => {
+    const persistedList = getState().listReducer.persistedList;
+    const currentList = getState().listReducer.lists[persistedList];
+    const tasks = currentList.tasks;
+
+    const affected = checkCurrent(data);
+
+    const s = tasks[affected].filter((task, idx) => {
+      return task !== data;
+    });
+
+    console.log(s);
+
+    tasks[affected] = s;
+
+    console.log(tasks);
+
+    dispatch({
+      type: t.DELETE_TASK,
+      data: [...tasks[0], ...task[1], ...task[2]],
+    });
+  };
+};
