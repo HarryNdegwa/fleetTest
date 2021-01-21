ecc7839e332e93d19935c430bb1d4aaf501fd61d
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-21 03:26:41 +0300

    Refactored some code in saved lists component

--- src/components/SavedLists/index.js
+++ src/components/SavedLists/index.js
@@ -10,20 +10,20 @@ function SavedLists(props) {
   return (
     <div className="saved-lists">
       <div className="saved-list-top-header">
-        <p className="mr-3">
+        <Link to="/account" className="link mr-3">
           <span className="mx-1">
             <FaUser />
           </span>
           mike@gmail.com
-        </p>
+        </Link>
 
-        <Link to="/" className="db-link mr-3">
+        <Link to="/saved-lits" className="link mr-3">
           <span className="mr-1">
             <AiOutlineBars />
           </span>
           My Saved Lists
         </Link>
-        <Link to="/" className="db-link mr-3">
+        <Link to="/" className="link mr-3">
           Logout
         </Link>
       </div>
