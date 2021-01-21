ca4b84fd4ada261feac24d416f66618e37883447
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-21 03:47:06 +0300

    Added list reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -3,12 +3,16 @@ import { persistReducer } from "redux-persist";
 import storageSession from "redux-persist/lib/storage/session";
 
 import { loginReducer } from "./loginReducer";
+import { listReducer } from "./listReducer";
 
 const persistConfig = {
   key: "root",
   storage: storageSession,
 };
 
-const rootReducer = combineReducers({ loginReducer: loginReducer });
+const rootReducer = combineReducers({
+  loginReducer: loginReducer,
+  listReducer: listReducer,
+});
 
 export default persistReducer(persistConfig, rootReducer);
---
+++ src/redux/reducers/listReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  list: null,
+  starred: [],
+  normal: [],
+  checked: [],
+  persistedList: null,
+};
+
+export const listReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
