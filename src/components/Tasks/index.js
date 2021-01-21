3ebe306064aa03fa2b79f5be2585399efb3801d3
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-21 07:32:35 +0300

    Added handlechange and handlekeypress in tasks component

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -1,17 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import "./style.css";
 import Task from "../Task";
 
 function Tasks(props) {
+  const [task, setTask] = useState("");
   useEffect(() => {}, [props.tasks]);
   const { tasks } = props;
+
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  };
+
+  const handleKeyPress = (e) => {
+    if (task.length === 0) {
+      return;
+    }
+    if (e.key === "Enter") {
+    }
+  };
   return (
     <div className="tasks">
       <input
         type="text"
         placeholder="Write your next task here..."
         className="task-input"
+        onChange={handleChange}
+        onKeyPress={handleKeyPress}
       />
       {tasks &&
         tasks.map((task, idx) => {
