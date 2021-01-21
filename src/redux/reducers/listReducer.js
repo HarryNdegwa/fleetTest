8386b84dc2d1fed777437727c1ad25321fd23386
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-21 04:52:54 +0300

    Added list save reducer logic

--- src/redux/reducers/listReducer.js
+++ src/redux/reducers/listReducer.js
@@ -1,13 +1,18 @@
+import * as t from "../actions/actionsType";
+
 const initialState = {
   lists: null,
-  starred: [],
-  normal: [],
-  checked: [],
   persistedList: null,
 };
 
 export const listReducer = (state = initialState, action) => {
   switch (action.type) {
+    case t.SAVE_LIST:
+      return {
+        ...state,
+        lists: state.lists.push(action.data),
+        persistedList: state.lists.length,
+      };
     default:
       return state;
   }
