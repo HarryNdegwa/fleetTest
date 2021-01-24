ce4fb2595596ee4d07ee2f54fb03c257adb010bf
HarryNdegwa <harrisonndegwa65@gmail.com>
2021-01-24 17:40:18 +0300

    Added active sidebar link logic

--- src/components/TasksDashBoard/SideBar/index.js
+++ src/components/TasksDashBoard/SideBar/index.js
@@ -1,16 +1,44 @@
 import React from "react";
 import { connect } from "react-redux";
 import "./style.css";
+import { FaChevronRight } from "react-icons/fa";
 
 function Sidebar(props) {
   const { lists } = props;
+  const checkIfActive = (list) => {
+    if (list.id == props.persistedList.id) {
+      return true;
+    }
+    return false;
+  };
+
   return (
     <div className="sidebar">
       <div style={{ height: "70px" }}></div>
       <ul>
         {lists &&
           lists.map((list, idx) => {
-            return <li key={idx}>{list.listName}</li>;
+            return (
+              <li key={idx}>
+                <h6 className="sidebar-list-title">
+                  {list.listName}
+                  <span>
+                    {checkIfActive(list) ? (
+                      <span>
+                        <FaChevronRight />
+                      </span>
+                    ) : (
+                      <span
+                        className="badge"
+                        style={{ backgroundColor: "#333333" }}
+                      >
+                        10
+                      </span>
+                    )}
+                  </span>
+                </h6>
+              </li>
+            );
           })}
       </ul>
     </div>
